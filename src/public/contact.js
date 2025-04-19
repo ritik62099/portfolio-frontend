bd5a56cdc1b847c1a640995a50904282804491e6
ritik62099 <ritik841207@gmail.com>
2025-04-19 11:13:26 +0530

    update url

--- src/public/contact.js
+++ src/public/contact.js
@@ -16,8 +16,8 @@ export default function Contact() {
     setSuccess("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
-        // const response = await fetch("https://ritik-portfolio-w5af.vercel.app/api/contact", {
+      // const response = await fetch("http://localhost:5000/api/contact", {
+        const response = await fetch("https://portfolio-frontend-mey4.vercel.app//api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),

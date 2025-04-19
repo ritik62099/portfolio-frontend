e4ab03df9e89aad0d10b563ec87713381424f2ef
ritik62099 <ritik841207@gmail.com>
2025-04-19 13:58:42 +0530

    update url

--- src/public/contact.js
+++ src/public/contact.js
@@ -17,7 +17,7 @@ export default function Contact() {
 
     try {
       // const response = await fetch("http://localhost:5000/api/contact", {
-        const response = await fetch("https://portfolio-frontend-mey4.vercel.app//api/contact", {
+        const response = await fetch("https://portfolio-frontend-mey4.vercel.app/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
